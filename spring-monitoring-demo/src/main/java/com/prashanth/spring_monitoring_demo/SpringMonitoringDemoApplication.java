9507a4953a62628e7a613d7d94186bd60ac32463
Prashanth Charla <prashanth.charla@thoughtworks.com>
2026-06-02 15:24:31 +0530

    Create spring boot app & configure actuator & prometheus endpoint

---
+++ spring-monitoring-demo/src/main/java/com/prashanth/spring_monitoring_demo/SpringMonitoringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring_monitoring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonitoringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMonitoringDemoApplication.class, args);
+	}
+
+}
---
+++ spring-monitoring-demo/src/test/java/com/prashanth/spring_monitoring_demo/SpringMonitoringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring_monitoring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonitoringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
